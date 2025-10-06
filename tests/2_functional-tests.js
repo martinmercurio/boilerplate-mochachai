b73b10ddf8af1004e0538b2e6a8eba787f9e91d4
martinmercurio <martin.mercurio1996@gmail.com>
2025-10-06 14:11:21 -0300

    bug changes

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -65,10 +65,11 @@ Browser.site = 'http://0.0.0.0:3000';
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
+const browser = new Browser();
+
 suiteSetup(function(done) {
   return browser.visit('/', done);
 });
-
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
@@ -78,7 +79,7 @@ suiteSetup(function(done) {
 
   suite('"Famous Italian Explorers" form', function () {
 
-    
+
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
       browser.fill('surname', 'Colombo').then(() => {
@@ -88,6 +89,8 @@ suiteSetup(function(done) {
       assert.equal(browser.text('span#surname'), 'Colombo');
       assert.equal(browser.elements('span#dates').length, 1);
       done();
+    });
+    });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
@@ -99,4 +102,5 @@ suiteSetup(function(done) {
     done();
     });
   });
+});
 });
