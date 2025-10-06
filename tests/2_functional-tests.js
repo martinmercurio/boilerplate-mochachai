d1df0704ea452761339f33821276b7d2191415c5
martinmercurio <martin.mercurio1996@gmail.com>
2025-10-06 14:04:01 -0300

    changes unit test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,8 +16,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -26,10 +26,10 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .keepOpen()
-        .get('/hello?name=xy_z')
+        .get('/hello?name=Martin')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Martin');
           done();
         });
     });
@@ -41,25 +41,33 @@ suite('Functional Tests', function () {
         .put('/travellers')
 
         .end(function (err, res) {
-          assert.fail();
-
+          aassert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
+       assert.equal(res.status, 200);
+       assert.equal(res.type, 'application/json');
+       assert.equal(res.body.name, 'Giovanni');
+      assert.equal(res.body.surname, 'da Verrazzano');
       done();
     });
   });
 });
 
 const Browser = require('zombie');
+Browser.site = 'http://0.0.0.0:3000';
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
+suiteSetup(function(done) {
+  return browser.visit('/', done);
+});
 
 
   suite('Headless browser', function () {
@@ -69,17 +77,26 @@ suite('Functional Tests with Zombie.js', function () {
   });
 
   suite('"Famous Italian Explorers" form', function () {
+
+    
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
+      browser.fill('surname', 'Colombo').then(() => {
+      browser.pressButton('submit', function() {
+      assert.equal(browser.status, 200);
+      assert.equal(browser.text('span#name'), 'Cristoforo');
+      assert.equal(browser.text('span#surname'), 'Colombo');
+      assert.equal(browser.elements('span#dates').length, 1);
       done();
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci').pressButton('submit', function() {
+      assert.equal(browser.status, 200);
+      assert.equal(browser.text('span#name'), 'Amerigo');
+      assert.equal(browser.text('span#surname'), 'Vespucci');
+      assert.equal(browser.elements('span#dates').length, 1);
+    done();
     });
   });
 });
